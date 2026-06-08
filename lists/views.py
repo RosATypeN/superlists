e60e663fda11d42024285a7f06d7196b13b4ecb6
YourName <your.email@example.com>
2026-06-08 17:38:19 +0800

    Add home page HTML and input box

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page(request):
+    return HttpResponse(
+        '<html>'
+        '<title>To-Do lists</title>'
+        '<body>'
+        '<form method="POST">'
+        '<input name="item_text">'
+        '</form>'
+        '</body>'
+        '</html>'
+    )
